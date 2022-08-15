fafd062887fba9e1cd97b6e82630abaddc2b4061
MacSI3D <MacSI3D@users.noreply.github.com>
2022-08-15 02:37:02 -0400

    Section 2.3 Button linked to Label text.

--- YouAreAwesome!/ViewController.swift
+++ YouAreAwesome!/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("👍 viewDidLoad has run!")
+        messageLabel.text = "Will it work? 🤔 "
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        print("😎 The message button was pressed!")
+        messageLabel.text = "Wow, this is cool"
+    }
+    
 }
 
